c03ee1f9ebe78a58ec884ec93ad6a6bac00964b1
Gabriela <gabrielaehrenbrink@gmail.com>
2023-11-23 16:31:28 +0000

    Finished Solo Project 1

---
+++ Projects/Solo_Projects/lib/take_away.py
@@ -0,0 +1,38 @@
+from lib.text_sender import Sms
+from lib.delivery_estimation import DeliveryEstimation
+
+class TakeAway:
+    def __init__(self):
+        self.menu = {'sandwich': 10, 'tea': 7, 'salad': 9, 'pizza': 20, 'coffee': 3, 'water': 1}
+        self.bill = 0
+        self.order = []
+
+    def read_menu(self):
+        formatted_menu = "Dishes:\t\t\t\t\t\t\tPrice:\n"
+
+        max_dish_length = max(len(dish) for dish in self.menu.keys())
+
+        for dish, price in self.menu.items():
+            formatted_menu += f"{dish.ljust(max_dish_length)}\t\t\t\t\t\t{price}£\n"
+
+        return formatted_menu
+
+    def add_to_order(self, client_order):
+        if client_order in self.menu:
+            price = self.menu[client_order]
+            self.bill += price
+            self.order.append(client_order)
+        else:
+            return f'Sorry, we are fresh out of {client_order} today. Please try something else.'
+
+    def receipt(self):
+        return f"You have ordered: {', '.join(self.order)}, your final bill is {self.bill}£"
+
+    def text_sender(self, account_sid, auth_token, from_number, client_number, message):
+        to_numbers = [client_number]
+        account_sid = 'ACa3f4836d8b69fee063ab09a13f95b0be'
+        auth_token = '7d9a86a9a3b650272bb7562fbebb1f8c'
+        from_number = '+447893938522'
+        message += f"\nEstimated delivery time: {DeliveryEstimation.get_one_hour_ahead_time()}"
+        Sms.send_sms(account_sid, auth_token, from_number, to_numbers, message)
+        return 'Text sent'
---
+++ Projects/Solo_Projects/tests/test_take_away.py
@@ -0,0 +1,42 @@
+from lib.take_away import TakeAway
+
+def test_read_takeaway_menu():
+    take_away = TakeAway()
+    result = take_away.read_menu()
+    assert result == 'Dishes:\t\t\t\t\t\t\tPrice:\nsandwich\t\t\t\t\t\t10£\ntea     \t\t\t\t\t\t7£\nsalad   \t\t\t\t\t\t9£\npizza   \t\t\t\t\t\t20£\ncoffee  \t\t\t\t\t\t3£\nwater   \t\t\t\t\t\t1£\n'
+
+def test_add_unavailable_item_to_order():
+    take_away = TakeAway()
+    take_away.add_to_order('tea')
+    assert take_away.receipt() == "You have ordered: tea, your final bill is 7£"
+
+def test_add_multiple_items_to_order():
+    take_away = TakeAway()
+    take_away.add_to_order('tea')
+    take_away.add_to_order('salad')
+    take_away.add_to_order('pizza')
+    assert take_away.receipt() == "You have ordered: tea, salad, pizza, your final bill is 36£"
+
+def test_add_repeated_items_to_order():
+    take_away = TakeAway()
+    take_away.add_to_order('tea')
+    take_away.add_to_order('tea')
+    take_away.add_to_order('pizza')
+    assert take_away.receipt() == "You have ordered: tea, tea, pizza, your final bill is 34£"
+
+
+def test_unavailable_item_to_order():
+    take_away = TakeAway()
+    result = take_away.add_to_order('banana')
+    assert result == 'Sorry, we are fresh out of banana today. Please try something else.'
+
+def test_text_sender():
+    take_away = TakeAway()
+    to_numbers = '+447307251340'
+    account_sid = 'ACa3f4836d8b69fee063ab09a13f95b0be'
+    auth_token = '7d9a86a9a3b650272bb7562fbebb1f8c'
+    from_number = '+447893938522'
+    message = 'example message'
+    take_away.text_sender(account_sid, auth_token, from_number, to_numbers, message)
+    result = 'Text sent'
+    assert result == 'Text sent'
